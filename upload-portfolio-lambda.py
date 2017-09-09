c1f329a9b44d258beeb5d8510874333fb70091b0
Dylan Sheffer <dsheffer16@gmail.com>
2017-09-09 11:39:24 -0400

    Updated deployment script with Code Pipeline

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,19 +4,34 @@ import StringIO
 import zipfile
 import mimetypes
 
+
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:879946027874:deployServerlessPortfolioTopic')
     
+    location = {
+        "bucketName": 'serverlessportfoliotutorialbuild',
+        "objectKey" : 'serverlessportfoliobuild.zip'
+    }
+    
     try:
+        job = event.get("CodePipline.job")
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+        
+        print "Building portfolio from " + str(location)
+        
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
     
         portfolio_bucket = s3.Bucket('serverlessportfoliotutorial')
-        build_bucket = s3.Bucket('serverlessportfoliotutorialbuild')
+        build_bucket = s3.Bucket(location["bucketName"])
         
         
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('serverlessportfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
     
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -26,6 +41,11 @@ def lambda_handler(event, context):
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
         print "Job done"
         topic.publish(Subject="Portfolio Deploy", Message="Portfolio Deployed Successfully!")
+        
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+        
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="The portfolio was NOT deployed successfully")
         raise
