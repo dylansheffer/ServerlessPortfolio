ce21cb32ac330391b6a6ab2b44584b352a5f28b7
Dylan Sheffer <dsheffer16@gmail.com>
2017-09-09 11:14:31 -0400

    Added basic exception handling to portfolio build script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,24 +4,30 @@ import StringIO
 import zipfile
 import mimetypes
 
-
-
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-
-    portfolio_bucket = s3.Bucket('serverlessportfoliotutorial')
-    build_bucket = s3.Bucket('serverlessportfoliotutorialbuild')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:879946027874:deployServerlessPortfolioTopic')
     
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
     
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('serverlessportfoliobuild.zip', portfolio_zip)
-
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj,nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    print "Job done"
+        portfolio_bucket = s3.Bucket('serverlessportfoliotutorial')
+        build_bucket = s3.Bucket('serverlessportfoliotutorialbuild')
+        
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('serverlessportfoliobuild.zip', portfolio_zip)
+    
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj,nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        print "Job done"
+        topic.publish(Subject="Portfolio Deploy", Message="Portfolio Deployed Successfully!")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The portfolio was NOT deployed successfully")
+        raise
     
     return 'Hello from Lambda'
